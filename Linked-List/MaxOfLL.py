a5f882eb8532b44c559152255df1c7a8dd5b66f3
yakshitgupta310 <65021783+yakshitgupta310@users.noreply.github.com>
2021-07-31 13:14:28 +0530

    Add files via upload

---
+++ Linked-List/MaxOfLL.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.next = None
+
+
+head = Node(1)
+head.next = Node(2)
+head.next.next = Node(3)
+head.next.next.next = Node(10)
+head.next.next.next.next = Node(5)
+head.next.next.next.next.next = Node(12)
+head.next.next.next.next.next.next = Node(4)
+
+
+def maxofll(head):
+    if head is None:
+        return "None"
+    elif head.next is None:
+        return head.key
+
+    max = None
+    while head != None:
+        if max is None:
+            max = head.key
+        elif max < head.key:
+            max = head.key
+        head = head.next
+    return max
+
+
+print(maxofll(head))
